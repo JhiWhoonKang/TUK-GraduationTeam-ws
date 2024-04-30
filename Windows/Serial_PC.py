cee18130399d9b7611256e0116fb8e292c2b0e33
JhiWhoon Kang <kangjw0914@naver.com>
2024-04-30 22:21:38 +0900

    function
    add function

--- Windows/Serial_PC.py
+++ Windows/Serial_PC.py
@@ -30,82 +30,54 @@ except IOError as e:
     print("Teensy 어디")
     exit()
 
-
 RIGHT = 0
 LEFT = 0
-UP = 0
-DOWN = 0
 STOP = 0
 ACC_Z = 220
+
 PREV_RIGHT = 0
 PREV_LEFT = 0
 PREV_STOP = 0
+
 LEFT_TURN = 2
 RIGHT_TURN = 2
+
+PREV_DIRECTION = 0 # 이전 방향 상태 추가
+CURRENT_SPEED = 0 # 현재 속도 변수 추가
+CURRENT_DIRECTION = 0 # 현재 방향 상태 추가
 def get_gamepad_input(joystick):
-    global UP, DOWN, RIGHT, LEFT, STOP, joy, PREV_RIGHT, PREV_LEFT, PREV_STOP, LEFT_TURN, RIGHT_TURN
+    global UP, DOWN, RIGHT, LEFT, PREV_RIGHT, PREV_LEFT, STOP, CURRENT_SPEED, PREV_DIRECTION, CURRENT_DIRECTION, LEFT_TURN, RIGHT_TURN, joy
 
     for event in pygame.event.get():
         if event.type == pygame.JOYAXISMOTION:
             if event.axis == 0:
                 if -0.2 < event.value < 0.2:
-                    # 조이스틱 - 중앙
-                    joy.axisX = int(0)
-                    STOP = joy.axisX
-                    speed_mode_stop(3, ACC_Z)                    
-                    
+                    CURRENT_DIRECTION = 0
+                    STOP = 0
+                    CURRENT_SPEED = 0
+                    # speed_mode(3, 0, CURRENT_SPEED, ACC_Z)
                 else:
-                    # 조이스틱 - 기울어짐
-                    if event.value > 0: # CW
-                        RIGHT_TURN = 1 # 회전하고 있던 상태
+                    if event.value > 0:  # CW
                         joy.axisX = int(event.value * 100)
                         RIGHT = joy.axisX
-
-                        if LEFT_TURN == 1:
-                            if speed_mode_stop(3, ACC_Z):
-                                speed_mode(3, 0, RIGHT, ACC_Z)
-                                RIGHT_TURN = 1
-                        else:
-                            speed_mode(3, 0, RIGHT, ACC_Z)
-                                
-                    elif event.value < 0: # CCW
-                        LEFT_TURN = 1
+                        if RIGHT != PREV_RIGHT:
+                            PREV_RIGHT = RIGHT
+                            CURRENT_DIRECTION = 0  # 오른쪽 방향
+                            CURRENT_SPEED = RIGHT
+                            # speed_mode(3, CURRENT_DIRECTION, RIGHT, ACC_Z)
+                        
+                    elif event.value < 0:  # CCW
                         joy.axisX = abs(int(event.value * 100))
                         LEFT = abs(joy.axisX)
-
-                        if RIGHT_TURN == 1:
-                            if speed_mode_stop(3, ACC_Z):
-                                speed_mode(3, 1, LEFT, ACC_Z)
-                                LEFT_TURN = 1
-                        else:
-                            speed_mode(3, 1, LEFT, ACC_Z)
-
-def speed_mode_stop(canid, acc):
-    global LEFT_TURN, RIGHT_TURN
-    crcbyte = (canid + 0xF6 + acc) & 0xFF
-    packet = [canid, 0xF6, 0, 0, acc, crcbyte]
-    teensy.write(bytearray(packet))
-    while True:
-        print("loop..")
-        if teensy.in_waiting > 0:
-            data = teensy.read(teensy.in_waiting)
-            print(data)
-            if data == b'\x03\xf6\x02\xfb':
-                print("stoip")
-                return True
-            elif data == b'\x03\xf6\x00\xf9':
-                print("help")
-                LEFT_TURN = 0
-                RIGHT_TURN = 0
-                break
-            elif data == b'\x03\xf6\x01\xfa':
-                break
-            else:
-                teensy.write(bytearray(packet))
-                break
-        else:
-            print("helpppppppppppppppppppppp")
-            break
+                        if LEFT != PREV_LEFT:
+                            PREV_LEFT = LEFT
+                            CURRENT_DIRECTION = 1  # 왼쪽 방향
+                            CURRENT_SPEED = LEFT
+                            # speed_mode(3, CURRENT_DIRECTION, LEFT, ACC_Z)
+        if event.type == pygame.JOYBUTTONUP:
+            if event.button == 0:
+                
+            
 
 def speed_mode(canid, dir, speed, acc):
     upper_speed = (speed >> 4) & 0x0F
@@ -115,16 +87,43 @@ def speed_mode(canid, dir, speed, acc):
     packet = [canid, 0xF6, byte2, lower_speed, acc, crcbyte]
     teensy.write(bytearray(packet))
 
+def speed_mode_stop(canid, acc):
+    global LEFT_TURN, RIGHT_TURN
+    crcbyte = (canid + 0xF6 + acc) & 0xFF
+    packet = [canid, 0xF6, 0, 0, acc, crcbyte]
+    teensy.write(bytearray(packet))
+    
+def read_encoder(canid):
+    byte1 = 31
+    crcbyte = (canid + byte1) & 0xFF
+    packet = [canid, 2, byte1, crcbyte]
+    teensy.write(bytearray(packet))    
 
-gamepad = init_gamepad()
+def read_pulse(canid):
+    byte1 = 33
+    crcbyte = (canid + byte1) & 0xFF
+    packet = [canid, 2, byte1, crcbyte]
+    teensy.write(bytearray(packet))
+    
+def go_home(canid):
+    byte1 = 91
+    crcbyte = (canid + byte1) & 0xFF
+    packet = [canid, 2, byte1, crcbyte]
+    teensy.write(bytearray(packet))
+
+def set_current_axis_to_zero(canid):
+    byte1 = 92
+    crcbyte = (canid + byte1) & 0xFF
+    packet = [canid, 2, byte1, crcbyte]
+    teensy.write(bytearray(packet))
+     
 try:     
+    gamepad = init_gamepad()
     if gamepad:
         while True:
             get_gamepad_input(gamepad)
-            
-            # if teensy.in_waiting > 0:
-            #     data = teensy.read(teensy.in_waiting)
-            #     print(data)                                
-            
+            speed_mode(3, CURRENT_DIRECTION, CURRENT_SPEED, ACC_Z)
+            print("DIR: {}, RIGHT: {}, LEFT: {}, ACC: {}, CURRENT SPEED: {}".format(CURRENT_DIRECTION, RIGHT, LEFT, ACC_Z, CURRENT_SPEED))
+            time.sleep(0.1)
 except KeyboardInterrupt:
     print("종료")
